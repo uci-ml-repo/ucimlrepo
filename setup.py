d152439bcefb427b1eab92410d497ac321920a45
Catarina Belém <PastelBelem8@users.noreply.github.com>
2023-07-27 08:52:36 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='ucimlrepo',
-      version='0.1',
+setup(
+      name='ucimlrepo',
+      version='0.0.1',
       description='A Python interface to import datasets from the UCI Machine Learning Repository',
       url='https://github.com/uci-ml-repo/ucimlrepo',
       author='Philip Truong',
       author_email='philtr928@example.com',
       license='MIT',
-      packages=['ucimlrepo'],
-      zip_safe=False)
+      packages=find_packages(exclude=("tests", "test.*", "*test*")),
+)
