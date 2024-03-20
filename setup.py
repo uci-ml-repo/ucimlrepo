9a07347e4bf8bc993ae2c081752d2f5d61daec35
Philip Truong <philtr928@gmail.com>
2024-03-20 00:13:35 -0700

    version no

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
       name='ucimlrepo',
-      version='0.0.5',
+      version='0.0.6',
       description='A Python interface to import datasets from the UCI Machine Learning Repository',
       url='https://github.com/uci-ml-repo/ucimlrepo',
       author='Philip Truong',
